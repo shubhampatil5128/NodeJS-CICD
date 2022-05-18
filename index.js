6c7e8276e53074b6a30b9788498d051c97d1eece
shubhampatil5128 <31090767+shubhampatil5128@users.noreply.github.com>
2022-05-18 17:59:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,11 +5,17 @@ app.get('/', function (req, res) {
     res.send('{ "response": "The application is working fine" }');
 });
 
-app.get('/will', function (req, res) {
-    res.send('{ "response": "The CICD pipeline for NodeJS application" }');
+app.get('/offer', function (req, res) {
+    res.send('{ "response": "samsung":"Samsung 10% Discount","adidas":"Adidas Shoe 70% Off","nikeshoe":"Nike Sports Shoe 50% off" }');
 });
-app.get('/ready', function (req, res) {
-    res.send('{ "response": " Great!, It works!" }');
+app.get('/shoe', function (req, res) {
+    res.send('{ "response": "tommy":"Tommy Hilfiger Shoe","adidas":"Adidas Running Shoe","nikeshoe":"Nike Sports Shoe" }');
+});
+app.get('/wishlist', function (req, res) {
+    res.send('{ "response": "1": "Apple Iphone", "2": "MacBook", "3": "Your Fav Something else"}');
+});
+app.get('/cart', function (req, res) {
+    res.send('{ "response":"data":[{"itemNo":1,"item":"Nike Shoe"},{"itemNo":2,"item":"Tommy Hilfiger Shirt"},{"itemNo":3,"item":"Calvin Klien Trousers" }');
 });
 app.listen(process.env.PORT || 3000);
 module.exports = app;
